57127a4ab1d250e426177481e6eb646e2cb1eeb9
EIMY <2365349265@qq.com>
2023-03-04 20:37:48 +0800

    upload lab00-02

---
+++ Lab/lab01-ok/tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    assert k >= 0
+    factorial = 1
+    while n > n-k:
+        factorial *= n
+        n -= 1
+        return factorial
